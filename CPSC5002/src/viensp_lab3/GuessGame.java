d93f7e41d9fdd53f85aa18f14ae4877f2731a7ed
Phillip J Viens <phillip.viens@gmail.com>
2017-07-20 13:08:19 -0700

    created getNewInteger method

--- CPSC5002/src/viensp_lab3/GuessGame.java
+++ CPSC5002/src/viensp_lab3/GuessGame.java
@@ -1,10 +1,30 @@
 package viensp_lab3;
 
+import java.util.Random;
+
 public class GuessGame {
-	
+	private int loRange;
+	private int hiRange;
+	private int newInteger;
 	
 	public GuessGame (int INT_LO, int INT_HI) {
 		
+		loRange = INT_LO;
+		hiRange = INT_HI;
+		newNumber();
 	}
+	
+	public void newNumber() {
+		//Creates a random object
+		Random rand = new Random();
+		
+		newInteger = rand.nextInt((hiRange*2) +1) - loRange;
+	}
+	
+	public int getNewInteger() {
+		
+		return newInteger;
+	}
+	
 
 }
