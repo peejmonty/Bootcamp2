779b9fa461ab3dbbf67d6ffd09b132c8ccb97208
Phillip J Viens <phillip.viens@gmail.com>
2017-07-24 14:27:56 -0700

    Still need to test MaxNumberOfPlays

--- CPSC5002/src/viensp_p1/P1.java
+++ CPSC5002/src/viensp_p1/P1.java
@@ -1,29 +1,53 @@
 package viensp_p1;
-
+/*
+* CPSC 5002, Seattle University
+* This is free and unencumbered software released into the public domain
+*/
 import java.util.Scanner;
+/**
+ * This is a class that calls the class TicTacToe. 
+ * Using instance fields in that class it plays the game.
+ * @author Phillip J Viens
+ *
+ */
 public class P1 {
 
-	public static void main(String[] args) {
-		
-		Scanner kbd = new Scanner(System.in);
-		int MAX = 3;
-		String input;
-		char repeat;
-		String[][] gameboardArray = new String[MAX][MAX];
-		System.out.println("Beginning");
-		TicsTacsToes game = new TicsTacsToes(kbd, MAX);
-		
-		game.welcomeMessage();
-		do{
-		game.gamePlay(kbd, gameboardArray);
-		System.out.println("Repeat Game? (y/n): ");
-		input = kbd.nextLine();
-		repeat = input.charAt(0);
-		} while (repeat == 'y' || repeat == 'Y');
-		game.goodbyeMessage();
-	}
+		/**
+		 * main method that sets variables for TicTacToe class. 
+		 * @param args
+		 */
+		public static void main(String[] args) {
+			
+			//Creates scanner for user input
+			Scanner kbd = new Scanner(System.in);
+			
+			//String to hold repeat variable
+			String input;
+			char repeat;
+			
+			//Creates an array to hold player's moves.
+			int MAX = 3;
+			String[][] gameboardArray = new String[MAX][MAX];
+			
+			//creates an object to call TicTacToe class
+			TicTacToe game = new TicTacToe(kbd, MAX);
+			
+			//calls welcome message from game instance.
+			game.welcomeMessage();
+			
+			// do/while that plays TicTacToe as many times as desired. 
+			do{
+				//Calls gamePlay method in TicTacToe class
+				game.gamePlay(kbd, gameboardArray);	
+				
+				//Asks user to repeat game
+				System.out.print("Repeat Game? (y/n): ");
+				input = kbd.nextLine();
+				repeat = input.charAt(0);
+			} while (repeat == 'y' || repeat == 'Y');
+			
+			//calls welcome message from game instance.
+			game.goodbyeMessage();
+			kbd.close();	//closes the Scanner.
+		}			
 }
-		
-		
-
-
--- CPSC5002/src/viensp_p1/TicTacToe.java
+++ CPSC5002/src/viensp_p1/TicTacToe.java
@@ -1,256 +1,324 @@
+package viensp_p1;
 /*
 * CPSC 5002, Seattle University
 * This is free and unencumbered software released into the public domain
 */
-package viensp_p1;
-
 import java.util.Scanner;
 
 /**
- * Create a TicTacToe Board
- * Have users take turns placing X's and O's
- * Check for a winner.
+ * 
  * @author Phillip J Viens
  *
  */
- public class TicTacToe {
-
-	String X;
-	String O;
-	int row;
-	int col;
-	String[][] gameboardArray;
-	String[] XsOs;
-	Scanner kbd;
+public class TicTacToe {
+	public String[][] gameboardArray;
+	public Scanner kbd;				//holds the Scanner
+	public  int MAX;				//hold the MAX number of rows & cols.
+	private int NEWMAX;				//holds MAX for checkBottomToTop method.
+	private int MaxNumberOfPlays;	//holds MAX for checkForNull method
+	private int playTurnCounter;	//counts the current play for modulo.
+	private String player;			//Holds the string for Xs & Os.
+	private boolean flag;			//holds the boolean var for do/while
+	private int row;				//holds the row variable for player move.
+	private int col;				//holds the col variable for player move.
 	
 	/**
 	 * The Constructor. Will take in the size of the TicTacToe Board.
-	 * have a static game board.
+	 * 
 	*/
-	 
-	
-	public TicTacToe(String[] players) {
-		final int MAX = 3;
-		XsOs = players;
-		//X = playerX;
-		//O = playerO;
-		String[][] gameboardArray = new String[MAX][MAX];
-		Scanner kbd = new Scanner(System.in);
-		
-		play(kbd, gameboardArray, XsOs);
-		
+	public TicTacToe(Scanner kbd, int MAX) {
+		this.kbd = kbd;		//creates new variable rules out shadowing.
+		this.MAX = MAX;		//creates new variable rules out shadowing.
 	}
 	
-	public String[][] play(Scanner kbd, String[][] gameboardArray, String[] XsOs) {
+	/**
+	 * gamePlay method. Counts the number of turns up to 9 turns switching 
+	 * between users each time. Using the CheckForWinner method. 
+	 * The board is monitored for winner. 
+	 * @param kbd is a scanner that allows user to input the move
+	 * @param gameboardArray takes in the array of the gameboard moves
+	 */
+	public void gamePlay(Scanner kbd, String[][] gameboardArray) {
+		playTurnCounter = 0;
+		MaxNumberOfPlays = (MAX*MAX);
 		
-		for (int i = 0; i < XsOs.length; i++)
-		{
-			System.out.println(XsOs[i] + ", it's your turn.");
+		//creates new Array for every new game.
+		gameboardArray = new String[MAX][MAX];
+		System.out.println();
+		displayGameBoard(gameboardArray);
+		
+		//do/while that repeats as long as flag == false.
+		do {
+			//checks to see who's turn it is.
+			if (playTurnCounter % 2 == 0) {
+				player = "X";
+			}
+			else {
+				player = "O";
+			}
+			System.out.println(player + ", it's your turn.");
+			
+			//Has use input row and column they'd like to make a move in.
 			System.out.print("Which Row: ");
 			row = kbd.nextInt();
-		
-			System.out.print("Which Column: ");
+			System.out.print("Which col: ");
 			col = kbd.nextInt();
 			kbd.nextLine();
-			//gameboardArray[row][col] = XsOs[i];
 			
-			while (gameboardArray[row][col] == "X" || gameboardArray[row][col] == "O") {
-				
+			//Checks to see if space has already been played.
+			//repeats if it's not null.
+			while (gameboardArray[row][col] == "X" 
+					|| gameboardArray[row][col] == "O") {
 				System.out.println("Space already used... Cheater");
 				System.out.print("Which Row: ");
 				row = kbd.nextInt();
-			
 				System.out.print("Which Column: ");
 				col = kbd.nextInt();
 				kbd.nextLine();
-				//gameboardArray[row][col] = XsOs[i];
 			}
-			gameboardArray[row][col] = XsOs[i];
-
-			displayGameBoard(gameboardArray);
-		}
-		return gameboardArray;
-		
-	}
-	
-	public boolean searchRow(int row) {
-		}
-		
 			
-		}
-		for (int i = 0; i < MAX; i++) {
-			for (int l = 0; l < MAX; l++) {
-				if (gameboardArray[i][l]
-						
-				
-		
-		
-			}
-		}
-		
-		return flag;
+			//sets player's move
+			gameboardArray[row][col] = player;
+			System.out.println();
+			
+			//shows updated gameboard
+			displayGameBoard(gameboardArray);
+			
+			//checks to see if winner has been found
+			//or if any space is null
+			flag = checkForWinner(playTurnCounter, MaxNumberOfPlays, 
+					gameboardArray, player);
+			playTurnCounter ++;
+		} while(flag == false);
 	}
-	/*
-	public String[][] playerO(Scanner kbd, String[][] gameboardArray) {
-		
-		System.out.println("O, it's your turn.");
-		System.out.print("Which Row: ");
-		row = kbd.nextInt();
-		
-		System.out.print("Which Column: ");
-		col = kbd.nextInt();
-		kbd.nextLine();
-		
-		
-		gameboardArray[row][col] = O;
-		
-		return gameboardArray;
-	}
-	*/
 	
-	public void displayGameBoard(String[][] gameBoardArray) {
-		int i;
-		int l;
+	/**
+	 * displays the TicTacToe grid and all moves made within the grid.
+	 * @param gameboardArray takes in the Array of moves by each player
+	 */
+	public void displayGameBoard(String[][] gameboardArray) {
+		int i;	//variables for loops
+		int l;	//variables for loops
 		System.out.print(" ");
-		for (i = 0; i < 3; i++)
+		
+		// prints the column number
+		for (i = 0; i < MAX; i++)
 		{
 			System.out.printf("%4s", i);
 		}
 		System.out.println();
-				
-		for(i = 0; i < 3; i++) {
+		
+		// prints the row numbers
+		for(i = 0; i < MAX; i++) {
 			System.out.print(i + ".|");
 
-			for(l = 0; l < 3; l++) {
-				if (gameBoardArray[i][l] == null)
+			// prints the rows including spaces and gameboardArray.
+			for(l = 0; l < MAX; l++) {
+				
+				//checks to see if any spaces are null.
+				if (gameboardArray[i][l] == null)
 				{
 					System.out.print("   |");
 				}
 				else
 				{
-				System.out.print(" " + gameBoardArray[i][l] + " |");
+				System.out.print(" " + gameboardArray[i][l] + " |");
 				}
 			}
 			System.out.println();
 			System.out.print("  -");
-			for(l = 0; l < 3; l++) {
+			for(l = 0; l < MAX; l++) {
 				System.out.print("----");
 			}
 			System.out.println();
 		}
 	}
- }
-	
-		/*
-		//displayGameBoard();
-		player = Player;
-		row = row();
-		col = col();
-		playsArray = playsArray();
-		//gameboardArray = gameboardArray(player, row, col);
-		
-		displayGameBoard(gameboardArray);
-		//displayGameBoard(MAX, playsArray);
-		//col();
-		//playsArray();
-		//displayGameBoard(MAX, playsArray);
-	}
-	/**
-	 * Takes in a users input for the row they'd like to place their turn
-	 * @return row variable to place the game piece.
-	 */
-	 /*
-	public int row(){
-		
-		Scanner kbd = new Scanner(System.in);
-		System.out.println();
-		//displayGameBoard();
-		System.out.println();
-		System.out.println(player + ", it is your turn");
-		System.out.println("Which Row? ");
-		row = kbd.nextInt();
-		kbd.nextLine();
-		
-		return row;
-		
-		
-	}
 	
 	/**
-	 * Takes in a users input for the column they'd like to place their turn
-	 * @return col variable to place the game piece
-	 */
-	 /*
-	public int col() {
-		Scanner kbd = new Scanner(System.in);
-		System.out.println("Which Column? ");
-		col = kbd.nextInt();
-		kbd.nextLine();
-		return col;
-		
-	}
-	
-	/**
-	 * This is an array method that takes in the player
-	 * as well as the column and row they'd like to place
-	 * @return the player's array for their own piece.
-	 */
-	 
-	 /*
-	public String[][] playsArray() {
-		row = row();
-		col = col();
-		final int MAX = 3;
-		String[][] playsArray = new String [MAX][MAX];
-		playsArray[row][col] = player;
-		
-		return playsArray;
-	}
-	
-	/**
-	 * a static array that takes in the the player as well as the column and row. 
-	 * @param playsArray returns a static 
-	 * game board array that contains both players
-	 * @return
-	 */
-	 /*
-	public static String[][] gameboardArray(String player, int row, int col) {
-		final int MAX = 3;
-		String[][] gameboardArray = new String[MAX][MAX];
-		
-		gameboardArray[row][col] = player;
-		
-		return gameboardArray;
-	}
-	
-	/**
-	 * Method that displays the game board
+	 *  Method that passes variables to other check methods. This determines 
+	 *  if a winner has been found after each turn.
+	 * @param playTurnCounter  Counter variable that is passed to checkForNull
+	 * @param MaxNumberOfPlays 
 	 * @param gameboardArray
+	 * @param player
+	 * @return boolean true/false
 	 */
-	 /*
-	
-	/**
-	 * intakes the Max from P1. and sets it in the constructos
-	 * @param squaresNumber as final int MAX
-	 */
-	 
-	 /*
-	public void setMAX(int squaresNumber) {
-		MAX = squaresNumber;
-	}
-	
-	/**
-	 * Gets the player array and sends and returns it.
-	 * @return
-	 */
-	/*
-	public String[][] getArray() {
-		return playsArray;
+	public boolean checkForWinner(int playTurnCounter, int MaxNumberOfPlays, 
+			String[][] gameboardArray, String player) {
 		
+			//calls checkRows method returns boolean.
+			if (checkRows(gameboardArray, player)){
+				winnerMessage();
+				return true;
+			}
+			
+			//calls checkColumns method returns boolean
+			else if(checkColumns(gameboardArray, player)) {
+				winnerMessage();
+				return true;
+			}
+			
+			//calls check diagonal TopToBottom method returns boolean.
+			else if(checkTopToBottom(gameboardArray, player)) {
+				winnerMessage();
+				return true;
+			}
+			
+			//calls check diagonal bottomToTop method returns boolean.
+			else if(checkBottomToTop(gameboardArray, player)) {
+				winnerMessage();
+				return true;
+			}
+			
+			//calls checkForNull method and returns boolean
+			else if(checkForNull()) {
+				catMessage();
+				return true;
+			}
+			else {
+				return false;
+			}
 	}
 	
-	//public String setPlayer(String Player) {
-	//	player = Player;
-	//	return player;
+	/**
+	 * boolean method that checks columns for a winner by summing a counter
+	 * If the counter equals the MAX number of columns. A winner is declared
+	 * @param gameboardArray takes in the gameboardArray to check it.
+	 * @param player Checks for the string of the player for it's presence.
+	 * @return boolean true/false
 	 */
+	public boolean checkColumns( String[][] gameboardArray, String player) {
+		int flagCounter;
+		for(int i = 0; i < MAX; i++) {
+			flagCounter = 0;
+			
+			//For loops that check through columns and rows.
+			for (int l = 0; l < MAX; l ++){
+				if (gameboardArray[l][i] == player) {
+					flagCounter ++;	
+				}
+				if (flagCounter == MAX) {
+					return true;
+				}
+			}
+		}		
+		return false;	
+	}
+	
+	/**
+	 * boolean method that checks rows for a winner by summing a counter
+	 * If the counter equals the MAX number of rows. A winner is declared
+	 * @param gameboardArray takes in the gameboardArray to check it.
+	 * @param player Checks for the string of the player for it's presence.
+	 * @return boolean true/false
+	 */
+	public boolean checkRows(String[][] gameboardArray, String player) {
+		int flagCounter = 0;
+		
+		//for loops that check through columns and rows.
+		for(int i = 0; i < MAX; i++) {
+			flagCounter = 0;
+			for (int l = 0; l < MAX; l ++){
+				if (gameboardArray[i][l] == player) {
+					flagCounter ++;	
+				}
+				if (flagCounter == MAX) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * boolean method that checks diagonal from 0,0 to 2,2 for a winner 
+	 * by summing a counter. If the counter equals the MAX number of the 
+	 * diagonal. A winner is declared.
+	 * @param gameboardArray takes in the gameboardArray to check it.
+	 * @param player Checks for the string of the player for it's presence.
+	 * @return boolean true/false
+	 */
+	public boolean checkTopToBottom(String[][] gameboardArray, String player) {
+		int flagCounter = 0;
+		int i;
+		for(i = 0; i < MAX; i++) {
+				if (gameboardArray[i][i] == player) {
+					flagCounter ++;	
+				}
+				if (flagCounter == MAX) {
+					return true;
+				}
+			}
+		return false;
+		}
+		
+	/**
+	 * boolean method that checks diagonal from 2,0 to 0,2 for a winner 
+	 * by summing a counter. If the counter equals the MAX number of the 
+	 * diagonal. A winner is declared.
+	 * @param gameboardArray takes in the gameboardArray to check it.
+	 * @param player Checks for the string of the player for it's presence.
+	 * @return boolean true/false
+	 */
+	public boolean checkBottomToTop(String[][] gameboardArray, String player) {
+		int flagCounter = 0;
+		NEWMAX = MAX - 1;
+		int i;
+		for (i = NEWMAX; i >= 0; i--) {
+			if (gameboardArray[NEWMAX-i][i] == player) {
+				flagCounter ++;
+			}
+			if (flagCounter == MAX) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * boolean method that counts number of plays against MAxNumberOfPlays.
+	 * If the board is full. It returns a false variable and ends the game.
+	 * @return boolean true/false
+	 */
+	public boolean checkForNull() {
+		
+			//Checks to see if maxNumber of plays has been met.
+			if(playTurnCounter == (MaxNumberOfPlays-1)) {
+				return true;
+			}
+			return false;
+		}
+		
+	/**
+	 * method that is called if a winner has been found.
+	 */
+	public void winnerMessage() {
+		System.out.println();
+		System.out.println("Congratulations " + player + " won!");
+	}
+	
+	/**
+	 * method that is called if the board is full and no winner is found.
+	 */
+	public void catMessage() {
+		System.out.println();
+		System.out.println("I'm sorry to say it, but "
+				+ "nobody won this round... ");
+	}
+	
+	/**
+	 * Welcome message that is displayed at the beginning of the game.
+	 */
+	public void welcomeMessage() {
+		System.out.println("Welcome to TicTacToe");
+	}
+	/**
+	 * Goodbye message that is displayed if game is not repeated.
+	 */
+	public void goodbyeMessage() {
+		System.out.println();
+		System.out.println("Have a good one Sailor.");
+		System.out.println();
+	}
+}
 
